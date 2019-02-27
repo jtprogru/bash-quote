bfe8c1d8d8e5ca6a742b185a1786cc1e9b864d34
Mike_Went <git@meew.me>
2019-02-27 19:56:17 +0300

    Replace httpie with curl
    And other minor improvements

--- bash-quote.plugin.zsh
+++ bash-quote.plugin.zsh
@@ -1,9 +1,9 @@
 
 function bash-quote {
     emulate -L zsh
-    Q=$(http --default-scheme=https https://bash.im/random | iconv -c -f CP1251 -t UTF-8 | grep "<div class=\"text\">" | head -n 1)
+    Q=$(curl -s https://bash.im/random | iconv -c -f CP1251 -t UTF-8 | grep "<div class=\"text\">" | head -n 1)
 
-    TXT=$( echo $Q| grep "<div class=\"text\">" | head -n 1 | sed 's/<br>/\\n\\t/g' | sed -e "s/<[^>]*>//g" | sed -e "s/\&lt\;/</g" | sed -e "s/\&gt\;/>/g" | sed "s/\&quot\;/\"/g")
+    TXT=$(echo "$Q" | grep "<div class=\"text\">" | head -n 1 | sed -e 's/<br>/\\n\\t/g' -e "s/<[^>]*>//g" -e "s/\&lt\;/</g" -e "s/\&gt\;/>/g" -e "s/\&quot\;/\"/g")
 
-     [[ -n "$TXT" ]] && print -P "%F{5}${TXT}%f"
+    [[ -n "$TXT" ]] && print -P "%F{5}${TXT}%f"
 }
