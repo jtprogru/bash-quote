0071e00572d6e4721e5c5e6d352b9ff082cc39b6
Michael Savin <8199112+jtprogru@users.noreply.github.com>
2022-09-03 23:36:06 +0300

    Update bash-quote.plugin.zsh

--- bash-quote.plugin.zsh
+++ bash-quote.plugin.zsh
@@ -1,12 +1,12 @@
 
 function bash-quote {
     emulate -L zsh
-    Q=$(curl -s https://bash.im/random | iconv -c -f CP1251 -t UTF-8 | grep "<div class=\"text\">" | head -n 1)
+    Q=$(curl -s http://bashorg.org/random | iconv -c -f CP1251 -t UTF-8 | grep "<div class=\"text\">" | head -n 1)
 
     TXT=$(echo "$Q" | grep "<div class=\"text\">" | head -n 1 | sed -e 's/<br>/\\n\\t/g' -e "s/<[^>]*>//g" -e "s/\&lt\;/</g" -e "s/\&gt\;/>/g" -e "s/\&quot\;/\"/g")
 
     if [[ ! -n "$TXT" ]]; then
-        echo "Error: unable to parse bash.im/random response."
+        echo "Error: unable to parse http://bashorg.org/random/random response."
         exit 1
     fi
     
