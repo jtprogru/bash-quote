fff3a4845783b82ceb7f5568b366e1a2dc7adac8
Michael Savin <jtprogru@gmail.com>
2023-02-02 14:13:35 +0300

    upd: Upgrade for last HTML

--- bash-quote.plugin.zsh
+++ bash-quote.plugin.zsh
@@ -1,13 +1,12 @@
 
 function bash-quote {
     emulate -L zsh
-    Q=$(curl -s http://bashorg.org/random | iconv -c -f CP1251 -t UTF-8 | grep "<div class=\"text\">" | head -n 1)
+    Q=$(curl -s http://bashorg.org/random | iconv -c -f CP1251 -t UTF-8 | grep "<div class=\"quote\">" | head -n 1)
 
-    TXT=$(echo "$Q" | grep "<div class=\"text\">" | head -n 1 | sed -e 's/<br>/\\n\\t/g' -e "s/<[^>]*>//g" -e "s/\&lt\;/</g" -e "s/\&gt\;/>/g" -e "s/\&quot\;/\"/g")
+    TXT=$(echo "$Q" | grep "<div class=\"quote\">" | head -n 1 | sed -e 's/<br \/>/\\n\\t/g' -e "s/<[^>]*>//g" -e "s/\&lt\;/</g" -e "s/\&gt\;/>/g" -e "s/\&quot\;/\"/g")
 
     if [[ ! -n "$TXT" ]]; then
         echo "Error: unable to parse http://bashorg.org/random/random response."
-        exit 1
     fi
     
     if [[ "$1" == "--color" ]]; then
